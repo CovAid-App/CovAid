7c29158b6bbbb8dc2dcebfbc75265f97918a5526
[Boyuan Liu] <50649079+boyuan12@users.noreply.github.com>
2020-04-26 16:52:21 -0700

    bot update token

--- discord_bot.py
+++ discord_bot.py
@@ -49,4 +49,4 @@ async def on_message(message):
             await message.channel.send(str(get_response(resp)))
 
 # return the client using this client id
-client.run("NzAxMTg2MTkxOTQwMjU1Nzg1.XqDDYA.Lzkw-vjrKEPoTIfv7_WZ2hr3pAE")
+client.run("NzAxMTg2MTkxOTQwMjU1Nzg1.XqYd-w.UQBkrfHsS1s_6a7hNP1fpf5xrhw")
